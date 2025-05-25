254e0fd302512e8e2391097af56e0ea659d3fe11
walkerstack <build77stack@outlook.com>
2025-05-25 14:02:13 +0300

    update metadata on hosted page

--- apps/web/src/app/[domain]/page.tsx
+++ apps/web/src/app/[domain]/page.tsx
@@ -1,18 +1,15 @@
+import { cache } from "react";
 import { notFound } from "next/navigation";
 import Chat from "@/components/chat";
 import { HostingProvider } from "@/contexts/hosting-context";
+import { constructMetadata } from "@/lib/metadata";
 
 import { db } from "@agentset/db";
 
 import Header from "./header";
 
-export default async function CustomDomainPage({
-  params,
-}: {
-  params: Promise<{ domain: string }>;
-}) {
-  const { domain } = await params;
-  const hosting = await db.hosting.findFirst({
+const getHosting = cache(async (domain: string) => {
+  return await db.hosting.findFirst({
     where: {
       domain: {
         slug: domain,
@@ -30,6 +27,30 @@ export default async function CustomDomainPage({
       },
     },
   });
+});
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ domain: string }>;
+}) {
+  const { domain } = await params;
+  const hosting = await getHosting(domain);
+
+  if (!hosting) return {};
+
+  return {
+    title: constructMetadata({ title: hosting.namespace.organization.name }),
+  };
+}
+
+export default async function CustomDomainPage({
+  params,
+}: {
+  params: Promise<{ domain: string }>;
+}) {
+  const { domain } = await params;
+  const hosting = await getHosting(domain);
 
   if (!hosting) notFound();
 
