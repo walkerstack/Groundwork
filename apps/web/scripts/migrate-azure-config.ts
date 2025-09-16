1fcd43ea453449c4f54f29d0130b25bcab13774e
walkerstack <build77stack@outlook.com>
2025-09-17 01:18:01 +0300

    fix azure migration script

--- apps/web/scripts/migrate-azure-config.ts
+++ apps/web/scripts/migrate-azure-config.ts
@@ -12,39 +12,45 @@ const transformBaseUrlToResourceName = (baseUrl: string) => {
   return baseUrl.replace("https://", "").replace("http://", "").split(".")[0]!;
 };
 
-export const migrateAzureConfig = async () => {
-  const namespacesWithAzure = await db.namespace.findMany({
-    where: {
-      embeddingConfig: {
-        not: Prisma.AnyNull,
-        path: ["provider"],
-        equals: "AZURE_OPENAI",
-      },
+const namespacesWithAzure = await db.namespace.findMany({
+  where: {
+    embeddingConfig: {
+      not: Prisma.AnyNull,
+      path: ["provider"],
+      equals: "AZURE_OPENAI",
+    },
+  },
+});
+
+console.log(`Found ${namespacesWithAzure.length} namespaces with Azure config`);
+let i = 0;
+for (const namespace of namespacesWithAzure) {
+  console.log(
+    `[${++i} / ${namespacesWithAzure.length}] Migrating namespace ${namespace.id}`,
+  );
+
+  const embeddingConfig = namespace.embeddingConfig as unknown as z.infer<
+    typeof _OldAzureEmbeddingConfigSchema
+  >;
+  if (!embeddingConfig || embeddingConfig.provider !== "AZURE_OPENAI") {
+    continue;
+  }
+
+  const newConfig: PrismaJson.NamespaceEmbeddingConfig = {
+    provider: "AZURE_OPENAI",
+    model: embeddingConfig.model,
+    resourceName: transformBaseUrlToResourceName(embeddingConfig.baseUrl),
+    deployment: embeddingConfig.deployment,
+    apiKey: embeddingConfig.apiKey,
+    apiVersion: "preview",
+  };
+
+  await db.namespace.update({
+    where: { id: namespace.id },
+    data: {
+      embeddingConfig: newConfig,
     },
   });
+}
 
-  for (const namespace of namespacesWithAzure) {
-    const embeddingConfig = namespace.embeddingConfig as unknown as z.infer<
-      typeof _OldAzureEmbeddingConfigSchema
-    >;
-    if (!embeddingConfig || embeddingConfig.provider !== "AZURE_OPENAI") {
-      continue;
-    }
-
-    const newConfig: PrismaJson.NamespaceEmbeddingConfig = {
-      provider: "AZURE_OPENAI",
-      model: embeddingConfig.model,
-      resourceName: transformBaseUrlToResourceName(embeddingConfig.baseUrl),
-      deployment: embeddingConfig.deployment,
-      apiKey: embeddingConfig.apiKey,
-      apiVersion: "preview",
-    };
-
-    await db.namespace.update({
-      where: { id: namespace.id },
-      data: {
-        embeddingConfig: newConfig,
-      },
-    });
-  }
-};
+console.log("Done!");
