f1fc68030393c353cdb5f455e9d5c53b98463e3e
walkerstack <build77stack@outlook.com>
2025-09-23 15:13:55 +0300

    add missing openapi route

--- apps/web/src/lib/openapi/v1/ingest-jobs/index.ts
+++ apps/web/src/lib/openapi/v1/ingest-jobs/index.ts
@@ -4,6 +4,7 @@ import { createIngestJob } from "./create-job";
 import { deleteIngestJob } from "./delete-job";
 import { getIngestJobInfo } from "./get-job";
 import { listIngestJobs } from "./list-jobs";
+import { reingestIngestJob } from "./reingest-job";
 
 export const ingestJobsPaths: ZodOpenApiPathsObject = {
   "/v1/namespace/{namespaceId}/ingest-jobs": {
@@ -12,6 +13,7 @@ export const ingestJobsPaths: ZodOpenApiPathsObject = {
   },
   "/v1/namespace/{namespaceId}/ingest-jobs/{jobId}": {
     get: getIngestJobInfo,
+    post: reingestIngestJob,
     delete: deleteIngestJob,
   },
 };
