635c23f075fc620caa4788a67d5f6d67a6a63a2b
walkerstack <build77stack@outlook.com>
2025-10-20 08:49:03 +0300

    fix documents page

--- apps/web/src/app/app.agentset.ai/(dashboard)/[slug]/[namespaceSlug]/documents/page.tsx
+++ apps/web/src/app/app.agentset.ai/(dashboard)/[slug]/[namespaceSlug]/documents/page.tsx
@@ -1,10 +1,6 @@
 import DashboardPageWrapper from "@/components/dashboard-page-wrapper";
-import { curlExample, tsSdkExample } from "@/lib/code-examples/ingest";
-import { Code2Icon } from "lucide-react";
 
-import { Button } from "@agentset/ui/button";
-
-import ApiDialog from "../playground/api-dialog";
+import { ApiIngestModal } from "./api-ingest-modal";
 import { IngestModal } from "./ingest-modal";
 import JobsPageClient from "./page.client";
 
@@ -14,33 +10,7 @@ export default function DocumentsPage() {
       <div className="mb-10 flex gap-2">
         <IngestModal />
 
-        <ApiDialog
-          title="Ingest via API"
-          trigger={(props) => (
-            <Button variant="ghost" {...props}>
-              <Code2Icon className="size-4" />
-              Ingest via API
-            </Button>
-          )}
-          description={
-            <>
-              Use the API to ingest documents into the knowledge base. For
-              extended info, <br />
-              checkout the{" "}
-              <a
-                href="https://docs.agentset.ai/api-reference/endpoint/ingest-jobs/create"
-                target="_blank"
-                className="text-foreground underline"
-              >
-                docs
-              </a>
-            </>
-          }
-          tabs={[
-            { title: "cURL", code: curlExample },
-            { title: "Javascript", code: tsSdkExample },
-          ]}
-        />
+        <ApiIngestModal />
       </div>
 
       <JobsPageClient />
