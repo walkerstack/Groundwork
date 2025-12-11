0ec44b16de8aaca1d799fafe12ea00d19a585010
walkerstack <build77stack@outlook.com>
2025-12-11 15:15:52 +0200

    patch zod on dev mode & refactor enum extraction

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,3 +1,29 @@
+import { z } from "zod/v4";
+
+// only in development mode
+if (process.env.NODE_ENV === "development") {
+  const originalAdd = z.globalRegistry.add;
+
+  // terrible hack for vite's HMR:
+  // without this monkey-patch, zod will throw an error whenever editing a schema file that uses
+  // `.register` as it would try to re-register the schema with the same ID again
+  // with this patch, re-registering will just replace the schema in the registry
+  z.globalRegistry.add = (
+    schema: Parameters<typeof originalAdd>[0],
+    meta: Parameters<typeof originalAdd>[1],
+  ) => {
+    if (!meta.id) {
+      return originalAdd.call(z.globalRegistry, schema, meta);
+    }
+    const existingSchema = z.globalRegistry._idmap.get(meta.id);
+    if (existingSchema) {
+      z.globalRegistry.remove(existingSchema);
+      z.globalRegistry._idmap.delete(meta.id);
+    }
+    return originalAdd.call(z.globalRegistry, schema, meta);
+  };
+}
+
 export * from "./embedding-model";
 export * from "./llm";
 export * from "./llm/constants";
